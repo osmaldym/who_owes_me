88ef74238ae99edb589cfe864f1a0e0bd27fd3a7
osmaldym <osmaldy11checo@gmail.com>
2026-05-10 19:22:39 -0400

    new design for home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({
@@ -10,9 +11,84 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateFormat format = DateFormat("yyyy/MM/dd");
+  DateTime now = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home page'));
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsetsGeometry.symmetric(horizontal: 15),
+        child: Column(
+          spacing: 10,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Total due: \$23,000.00',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Row(
+              spacing: 5,
+              children: [
+                RawChip(
+                  avatar: const Icon(Icons.person),
+                  label: const Text('New user'),
+                  onPressed: () => print('New user'),
+                ),
+                RawChip(
+                  avatar: const Icon(Icons.money),
+                  label: const Text('New pay'),
+                  onPressed: () => print('New pay'),
+                ),
+              ],
+            ),
+            const Text(
+              'Next pays',
+              style: TextStyle(
+                fontSize: 26
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  for (var i = 0; i < 5; i++)
+                    ListTile(
+                      leading: const CircleAvatar(
+                        child: Icon(Icons.person),
+                      ),
+                      title: const Text('John Due'),
+                      trailing: const Text('\$50,000.00'),
+                      subtitle: Text(format.format(now)),
+                    ),
+                ],
+              )
+            ),
+            const Text(
+              'Users who owe the most',
+              style: TextStyle(
+                fontSize: 26
+              ),
+            ),
+            Card(
+              child: Column(
+                children: [
+                  for (var i = 0; i < 5; i++)
+                    const ListTile(
+                      leading: CircleAvatar(
+                        child: Icon(Icons.person),
+                      ),
+                      title: Text('John Due'),
+                      subtitle: Text('\$25,000.00'),
+                    ),
+                ],
+              )
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
