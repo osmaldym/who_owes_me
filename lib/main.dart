5eb77cc82c07f717774dac79d17804f68f787a23
osmaldym <osmaldy11checo@gmail.com>
2026-05-10 22:49:19 -0400

    adding go_router to project, creating root route and updating main for support it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:who_owes_me/screens/main_page.dart';
+import 'package:who_owes_me/router/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -32,7 +33,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MainPage(),
     );
   }
 }
---
+++ lib/router/router.dart
@@ -0,0 +1,13 @@
+
+import 'package:go_router/go_router.dart';
+import 'package:who_owes_me/router/route.dart';
+import 'package:who_owes_me/screens/main_page.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: Route.root,
+      builder: (context, state) => MainPage(),
+    )
+  ]
+);
