36200e6abaf2b8c3a577115456a6c4f3c643b44a
osmaldym <osmaldy11checo@gmail.com>
2026-05-11 23:41:06 -0400

    creating new user model

---
+++ lib/models/user.dart
@@ -0,0 +1,25 @@
+import 'package:who_owes_me/models/baseModel.dart';
+
+class User extends BaseModel {
+  String? name;
+  String? email;
+  String? phone;
+
+  User({
+    super.id,
+    this.email,
+    this.name,
+    this.phone
+  });
+
+  @override
+  Map<String, Object?> toMap() => {
+    ...super.toMap(),
+    'name': name,
+    'email': email,
+    'phone': phone,
+  };
+
+  @override
+  String toString() => 'User: ${toMap()}';
+}
