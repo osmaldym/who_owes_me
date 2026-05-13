bf8c56539c298f35e9cda41981c3c31554c167c6
osmaldym <osmaldy11checo@gmail.com>
2026-05-12 23:55:13 -0400

    creating new pay model

---
+++ lib/models/pay.dart
@@ -0,0 +1,28 @@
+import 'package:who_owes_me/models/baseModel.dart';
+
+class Pay extends BaseModel {
+  String? title;
+  int? userId;
+  double? amount;
+  DateTime? date;
+
+  Pay({
+    super.id,
+    this.userId,
+    this.title,
+    this.amount,
+    this.date,
+  });
+
+  @override
+  Map<String, Object?> toMap() => {
+    ...super.toMap(),
+    'user_id': userId,
+    'title': title,
+    'amount': amount,
+    'date': date?.microsecondsSinceEpoch,
+  };
+
+  @override
+  String toString() => 'Pay: ${toMap()}';
+}
