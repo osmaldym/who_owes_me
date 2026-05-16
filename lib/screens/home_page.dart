d480ed24442e7c455b871cbadd61ff4a04847223
osmaldym <osmaldy11checo@gmail.com>
2026-05-16 01:22:03 -0400

    overviews fully working with real data

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:who_owes_me/db/dao.dart';
+import 'package:who_owes_me/models/realated_pay.dart';
 import 'package:who_owes_me/router/route.dart' as app;
 import 'package:intl/intl.dart';
 
@@ -13,8 +15,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Dao dao = Dao();
   DateFormat format = DateFormat("yyyy/MM/dd");
+  NumberFormat nFormat = NumberFormat.decimalPatternDigits(locale: 'en_US', decimalDigits: 2);
   DateTime now = DateTime.now();
+
+  Future<double>? _totalDue;
+  Future<List<RelatedPay>>? _nextPays;
+  Future<List<Map<String, Object?>>>? _oweMost;
+
+  @override
+  void initState() {
+    _totalDue = dao.getTotalDue();
+    _nextPays = dao.getNextPays();
+    _oweMost = dao.getUsersOweMost();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +41,24 @@ class _HomePageState extends State<HomePage> {
           spacing: 10,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Total due: \$23,000.00',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-              ),
+            FutureBuilder(
+              future: _totalDue,
+              builder: (context, snapshot) {
+                double totalDouble = 0;
+                
+                if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+                  totalDouble = snapshot.data ?? 0;
+                }
+
+                String total = nFormat.format(totalDouble);
+                return Text(
+                    'Total due: \$$total',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  );
+              }
             ),
             Row(
               spacing: 5,
@@ -54,19 +82,32 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Card(
-              child: Column(
-                children: [
-                  for (var i = 0; i < 5; i++)
-                    ListTile(
-                      leading: const CircleAvatar(
-                        child: Icon(Icons.person),
-                      ),
-                      title: const Text('John Due'),
-                      trailing: const Text('\$50,000.00'),
-                      subtitle: Text(format.format(now)),
-                    ),
-                ],
-              )
+              child: FutureBuilder(
+                future: _nextPays,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) return const Center(child: CircularProgressIndicator());
+            
+                  if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+                    List<RelatedPay>? relatedPays = snapshot.data;
+
+                    return Column(
+                      children: [
+                        for (RelatedPay rPay in relatedPays!)
+                          ListTile(
+                            leading: const CircleAvatar(
+                              child: Icon(Icons.person),
+                            ),
+                            title: Text(rPay.user?.name ?? 'Unknown user'),
+                            trailing: Text('\$${nFormat.format(rPay.amount ?? 0)}'),
+                            subtitle: rPay.date != null ? Text(format.format(rPay.date!)) : null,
+                          )
+                      ],
+                    );
+                  }
+
+                  return const Text('No pays to show');
+                }
+              ),
             ),
             const Text(
               'Users who owe the most',
@@ -75,18 +116,31 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Card(
-              child: Column(
-                children: [
-                  for (var i = 0; i < 5; i++)
-                    const ListTile(
-                      leading: CircleAvatar(
-                        child: Icon(Icons.person),
-                      ),
-                      title: Text('John Due'),
-                      subtitle: Text('\$25,000.00'),
-                    ),
-                ],
-              )
+              child: FutureBuilder(
+                future: _oweMost,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) return const Center(child: CircularProgressIndicator());
+            
+                  if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+                    List<Map<String, Object?>>? relatedPays = snapshot.data;
+
+                    return Column(
+                      children: [
+                        for (Map<String, Object?> rPay in relatedPays!)
+                          ListTile(
+                            leading: const CircleAvatar(
+                              child: Icon(Icons.person),
+                            ),
+                            title: Text(rPay['name'] as String? ?? 'Unknown user'),
+                            subtitle: rPay['owe_total'] != null ? Text(nFormat.format(rPay['owe_total'] as double?)) : null,
+                          )
+                      ],
+                    );
+                  }
+
+                  return const Text('No pays to show');
+                }
+              ),
             ),
           ],
         ),
