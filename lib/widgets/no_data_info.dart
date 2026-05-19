7e7a3de9806061f689b066fc8ee2085119358b50
osmaldym <osmaldy11checo@gmail.com>
2026-05-18 23:32:01 -0400

    new component to show no data messages

---
+++ lib/widgets/no_data_info.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class NoDataInfo extends StatelessWidget {
+  String? text;
+  String? message;
+  bool showMessage;
+  IconData? icon;
+
+  NoDataInfo({
+    super.key,
+    this.text,
+    this.message,
+    this.icon,
+    this.showMessage = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsetsGeometry.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 10,
+          children: [
+            CircleAvatar(
+              radius: 38,
+              child: Icon(icon ?? Icons.money_off, size: 38,),
+            ),
+            Text(
+              text ?? "There's no data to show",
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w500
+              ),
+            ),
+            if (showMessage)
+              Text(
+                message ?? "Click the button below to add a new element",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey
+                ),
+              )
+          ],
+        ),
+      )
+    );
+  }
+}
